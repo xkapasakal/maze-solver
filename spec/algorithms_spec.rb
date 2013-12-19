1e45d01a893a6d1847ee745ca0b750d22b914bdb
Christos Kapasakalidis <xkapasakal@gmail.com>
2013-12-19 10:59:21 +0200

    Add one more test

--- spec/algorithms_spec.rb
+++ spec/algorithms_spec.rb
@@ -12,6 +12,16 @@ describe Maze do
           (point.is_marked_twice?).should be_true
         end
       end
+
+      maze_file = File.new('mazes/maze_without_end_4x4.txt')
+      maze = Maze.new(maze_file)
+      robot = Robot.new(maze)
+      robot.move_dfs Algorithms.new.dfs
+      maze.grid.each do |row|
+        row.select{|point| point.type != Point::TYPES[:wall]}.each do |point|
+          (point.is_marked_twice?).should be_true
+        end
+      end
     end
   end
 end
